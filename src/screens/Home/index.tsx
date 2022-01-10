0206198d691d9b54a263b99283c824a68862244f
Marco Galindo <marco.galindo@meliuz.com.br>
2022-01-10 16:47:26 -0300

    criação do header e importacao das imagens

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Logo from '../../assets/logo.svg';
 import {
-  Container, Title,
+  Container,
+  Header,
+  TotaCars,
+  HeaderContent
 } from './styles';
 
 export function Home() {
   return (
     <Container>
-      <Title>
-        Home
-      </Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor='transparent'
+      />
+      <Header>
+        <HeaderContent>
+          <Logo
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotaCars>
+            Total de 12 carros
+          </TotaCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 }
